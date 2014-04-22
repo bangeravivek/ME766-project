7ad1f9b7831e8d6256fc0b31055c764372aa1ad3
Vivek Bangera <bangera.vivek@gmail.com>
2014-04-22 12:44:07 +0530

    Random sparse matrix generated will not be square now

--- randommat.py
+++ randommat.py
@@ -4,8 +4,9 @@ from random import randint
 
 f=open('matrix.txt','wt')
 X=randint(100,1000)
+W=randint(100,1000)
 for i in range(0,X):
-	for j in range(0,X):
+	for j in range(0,W):
 		Y=randint(1,100)
 		if Y>85:
 			Z=str(randint(1,100))
