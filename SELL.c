f4e4e9ae597090d139a90143d85aa128f6c8dc9c
Vivek Bangera <bangera.vivek@gmail.com>
2014-04-27 03:33:10 +0530

    Some work still to be done. Took care of seg fault

--- SELL.c
+++ SELL.c
@@ -91,7 +91,7 @@ void printmat(int** matrix, int N, int Nj)
 	for (i=0;i<N;i++)
 	{	
 		printf("\n");
-		for (j=0;j<Nj;j++)
+		for (j=0;j<N;j++)
 		{
 			printf("%d ",matrix[i][j]);
 		}
@@ -99,6 +99,43 @@ void printmat(int** matrix, int N, int Nj)
 	printf("\n");
 }
 
+void printtofile(int** matrix, int K, char* filename)
+{
+	/*
+	Prints original 2D matrices to file
+	*/
+	FILE *fp;
+	fp=fopen(filename,"wt");
+	int i,j;
+	for (i=0;i<K;i++)
+	{
+		fprintf(fp, "\n");
+		for (j=0;j<K;j++)
+		{
+			fprintf(fp, "%d\t", matrix[i][j]);
+		}
+	}
+}
+
+void printtofile1D(int* matrix, int K, char* filename)
+{
+	/*
+	Prints resultant matrix to a file
+	*/
+	FILE *fp;
+	fp=fopen(filename,"wt");
+	int i,j;
+	int counters=0;
+	for (i=0;i<K;i++)
+	{
+		fprintf(fp, "\n");
+		for (j=0;j<K;j++)
+		{
+			fprintf(fp, "%d \t", matrix[counters]);
+			counters++;
+		}
+	}
+}
 
 int* Make1DIntArray(int arraySizeX) {
 int* theArray;
@@ -122,9 +159,9 @@ void freese(int sizeX, int sizeY, double** ptr)
 
 void main()
 {
-	const int N=6;
+	const int N=100;
 	
-	const int Dsize=50;
+	const int Dsize=1000;
 	FILE *arr, *vec;
 	int i,j,maxrowwidth=0,tint=0;
 	int** a=Make2DIntArray(N,N);
@@ -137,8 +174,9 @@ void main()
 	int** sccol=Make2DIntArray(N,N);	//sell c col
 	int* rowwidth=Make1DIntArray(N);	//number of elements in each row
 	int* temp=Make1DIntArray(N);
+	int* cols=Make1DIntArray(N/2);
 	//int val[10],col[10],row[10];
-	arr=fopen("mat.txt","r");
+	arr=fopen("matrix100.txt","r");
 	int k=0,cinrow=0;
 	struct timeval start, end;
 	gettimeofday(&start, NULL);
@@ -146,7 +184,7 @@ void main()
 	
 	//Reading the vector
 	
-	vec=fopen("vec.txt","r");
+	vec=fopen("vector100.txt","r");
 	for (i=0;i<N;i++)
 	{
 		fscanf(vec,"%d",&vecX[i]);
@@ -183,24 +221,46 @@ void main()
 			if(a[i][j]!=0)
 			{
 				scval[i][k]=a[i][j];
+				//printf("\n scval[%d][%d]=%d",i,k,scval[i][k]);
+
+				//sleep(1);
 				sccol[i][k]=j;
+				//printf("\n sccol[%d][%d]=%d",i,k,sccol[i][k]);
 				rowwidth[i]=k+1;
 				if(rowwidth[i]>maxrowwidth)
-				{	printf("\nrow[%d] width=%d\n",i,maxrowwidth);
+				{	//printf("\nrow[%d] width=%d\n",i,maxrowwidth);
 					maxrowwidth=rowwidth[i];
 				}k++;
 			}
+				
 		}
+		//printf("\nRow width %d = %d", i, rowwidth[i]);
 		k=0;
 	}
+	
+	
+	
+	
 	for(i=0;i<N-1;i++)
 	{
-	for(j=0;j<N;j++)
+	for(j=0;j<N-1;j++)
 	{
 		if(rowwidth[j]<rowwidth[j+1])
 		{	printf("\nrow %d width=%d",j,rowwidth[j]);
 			temp=scval[j];
+			/*printf("\nscval[%d]=",j);
+			for(k=0;k<rowwidth[j];k++)
+			{
+				printf("%d ", scval[j][k]);
+			}
+			printf("\nscval[%d]=",j+1);
+			for(k=0;k<rowwidth[j+1];k++)
+			{
+				printf("%d ", scval[j+1][k]);
+			}
+			*/
 			scval[j]=scval[j+1];
+
 			scval[j+1]=temp;
 			temp=sccol[j];
 			sccol[j]=sccol[j+1];
@@ -213,13 +273,17 @@ void main()
 	}	
 		
 	printf("\nmaxrowwidth=%d\n",maxrowwidth);				
-	printmat(scval,N,maxrowwidth);
-	printmat(sccol,N,maxrowwidth);
+	printtofile(scval,N,"scval.txt");
+	printtofile(sccol,N,"sccol.txt");
 	printf("\n Vector is:\n");
 	for (i=0;i<N;i++)
 	{
 		printf("%d\n",vecX[i]);
 	}
+	
+	
+	/* NEED TO FIGURE OUT A WAY TO POPULATE cols SO AS TO HAVE varmat CREATED PROPERLY. SYSTEM CRASHES OTHERWISE
+	
 	
 	int cols[3]={4,3,3};
 	int** varmat=Changeto2DVariableIntArray(scval,N,N/2,2,cols);
